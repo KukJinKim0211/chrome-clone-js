640b614ed4a0c2b12d24607b0817d8e8ae998d60
KukjinKim0211 <rnrwls0211@naver.com>
2022-02-24 15:45:35 +0900

    배경변경

--- js/background.js
+++ js/background.js
@@ -1,7 +1,12 @@
 const images = [
-  "01.jpeg",
-  "02.jpeg",
-  "03.jpeg"
+  "노을.jpg",
+  "밤하늘옥상.jpg",
+  "벚꽃.jpg",
+  "선물.jpg",
+  "안드로메다.jpg",
+  "집콕.jpg",
+  "프렌즈반.jpg",
+  "해변서핑.jpg"
 ];
 const random = Math.floor(Math.random() * images.length)
 const chosenImage = images[random];
